2c168ce3729bd7795040d25e92fba21bcb7b1bed
Mikael Elenius <mikael.elenius@gmail.com>
2018-11-24 21:43:55 +0100

    update parameter description in dunif2()

--- R/dunif2.R
+++ R/dunif2.R
@@ -1,6 +1,6 @@
 #' @title Populationsantagande, två likformiga fördelningar
 #' @description Två likformiga fördelningar ihopsatta till en, ena delen barn (under 18 år) och andra delen vuxna (18 år eller över). Ger täthetsfunktionen. Se Figur 2.
-#' @param x age.
+#' @param x Ålder.
 #' @param min Lägsta ålder för barn, måste vara 18 år eller under. Förvalt är 15 år.
 #' @param max Högsta ålder, måste vara 18 år eller över. Förvalt är 21 år.
 #' @param prevalence Prevalens, andelen som är vuxna (18 år eller över). Förvalt är 0.84
